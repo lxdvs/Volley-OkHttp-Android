0c8457858e98cd6d14cf39faf5353d949a5854c3
Alex Davis <alex.davis@airbnb.com>
2013-07-15 14:23:17 -0700

    unfuckup exception logging nullcheck

--- src/com/android/volley/ExecutorDelivery.java
+++ src/com/android/volley/ExecutorDelivery.java
@@ -65,7 +65,8 @@ public class ExecutorDelivery implements ResponseDelivery {
 
     @Override
     public void postError(Request<?> request, VolleyError error) {
-        String errStr = error.networkResponse != null && TextUtils.isEmpty(error.networkResponse.errorResponseString) ? 
+        
+        String errStr = error.networkResponse == null || TextUtils.isEmpty(error.networkResponse.errorResponseString) ? 
                 "<unparsed>" : error.networkResponse.errorResponseString;
         request.addMarker("post-error: " + errStr);
         Response<?> response = Response.error(error);
