b041186738cd8dfd8cc6b27d327fab4926406da1
Alex Davis <alex.davis@airbnb.com>
2013-08-15 10:35:08 -0700

    Hide all volley logging behind debug flags

--- src/com/android/volley/VolleyLog.java
+++ src/com/android/volley/VolleyLog.java
@@ -36,23 +36,33 @@ public class VolleyLog {
     }
 
     public static void d(String format, Object... args) {
-        Log.d(TAG, buildMessage(format, args));
+        if (DEBUG) {
+            Log.d(TAG, buildMessage(format, args));
+        }
     }
 
     public static void e(String format, Object... args) {
-        Log.e(TAG, buildMessage(format, args));
+        if (DEBUG) {
+            Log.e(TAG, buildMessage(format, args));
+        }
     }
 
     public static void e(Throwable tr, String format, Object... args) {
-        Log.e(TAG, buildMessage(format, args), tr);
+        if (DEBUG) {
+            Log.e(TAG, buildMessage(format, args), tr);
+        }
     }
 
     public static void wtf(String format, Object... args) {
-        Log.wtf(TAG, buildMessage(format, args));
+        if (DEBUG) {
+            Log.wtf(TAG, buildMessage(format, args));
+        }
     }
 
     public static void wtf(Throwable tr, String format, Object... args) {
-        Log.wtf(TAG, buildMessage(format, args), tr);
+        if (DEBUG) {
+            Log.wtf(TAG, buildMessage(format, args), tr);
+        }
     }
 
     /**
