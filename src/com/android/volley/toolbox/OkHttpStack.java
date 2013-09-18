79040e6d179e8a16525f1db57943ddd81d591328
Alex Davis <alex.davis@airbnb.com>
2013-09-17 23:37:00 -0700

    redisable spdy to maybe help with native crashes

--- src/com/android/volley/toolbox/OkHttpStack.java
+++ src/com/android/volley/toolbox/OkHttpStack.java
@@ -8,28 +8,34 @@ import java.util.ArrayList;
 import com.squareup.okhttp.OkHttpClient;
 
 /**
- * An {@link com.android.volley.toolbox.HttpStack HttpStack} implementation which
- * uses OkHttp as its transport.
+ * An {@link com.android.volley.toolbox.HttpStack HttpStack} implementation which uses OkHttp as its transport.
  */
 public class OkHttpStack extends HurlStack {
-  private final OkHttpClient client;
+    private final OkHttpClient client;
 
-  public OkHttpStack() {
-    this(new OkHttpClient());
-  }
-
-  public OkHttpStack(OkHttpClient client) {
-    if (client == null) {
-      throw new NullPointerException("Client must not be null.");
+    public OkHttpStack() {
+        this(new OkHttpClient());
     }
-    this.client = client;
-  }
 
-  public OkHttpClient getClient() {
-      return client;
+    public OkHttpStack(OkHttpClient client) {
+        if (client == null) {
+            throw new NullPointerException("Client must not be null.");
+        }
+        this.client = client;
+
+        // No SPDY :( https://github.com/square/okhttp/issues/184
+        // tried URL.setURLStreamHandlerFactory(new OkHttpClient()); but not working atm
+        ArrayList< String > list = new ArrayList< String >();
+        list.add("http/1.1");
+        client.setTransports(list);
     }
-  
-  @Override protected HttpURLConnection createConnection(URL url) throws IOException {
-    return client.open(url);
-  }   
+
+    public OkHttpClient getClient() {
+        return client;
+    }
+
+    @Override
+    protected HttpURLConnection createConnection(URL url) throws IOException {
+        return client.open(url);
+    }
 }
