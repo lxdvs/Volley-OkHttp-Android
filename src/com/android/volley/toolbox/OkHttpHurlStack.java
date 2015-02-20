3c87221a90202553e5fa09d3bd4a8418e85d0e7e
Felipe Lima <felipe.lima@gmail.com>
2015-02-20 12:41:43 -0800

    Adds pure OkHttpStack

---
+++ src/com/android/volley/toolbox/OkHttpHurlStack.java
@@ -0,0 +1,43 @@
+package com.android.volley.toolbox;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * An {@link com.android.volley.toolbox.HttpStack HttpStack} implementation which uses OkHttp as its transport.
+ */
+public class OkHttpHurlStack extends HurlStack {
+    private final OkHttpClient client;
+
+    public OkHttpHurlStack() {
+        this(new OkHttpClient());
+    }
+
+    public OkHttpHurlStack(OkHttpClient client) {
+        if (client == null) {
+            throw new NullPointerException("Client must not be null.");
+        }
+        this.client = client;
+
+        /*
+        // force http
+        // no spdy https://github.com/square/okhttp/issues/963
+        ArrayList<Protocol> list = new ArrayList< Protocol >();
+        list.add(Protocol.HTTP_1_1);
+        client.setProtocols(list);
+        */
+    }
+
+    public OkHttpClient getClient() {
+        return client;
+    }
+
+    @Override
+    protected HttpURLConnection createConnection(URL url) throws IOException {
+        return new OkUrlFactory(client).open(url);
+    }
+}
